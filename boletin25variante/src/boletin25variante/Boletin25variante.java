2118ec372da7e7557a88df12fc53ab08bec5a7a1
antonio <antonio@192.168.0.11>
2017-06-05 12:42:45 +0200

    boletin25 variante clase ventana

---
+++ boletin25variante/src/boletin25variante/Boletin25variante.java
@@ -0,0 +1,14 @@
+
+package boletin25variante;
+
+
+public class Boletin25variante {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ventana obx= new Ventana();
+        obx.setVisible(true);
+    }
+    
+}
